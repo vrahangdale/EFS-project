785b003548971b923ca1f1176af978a874d54a21
Vaibhav Rahangdale <vrahangdale@unomaha.edu>
2017-02-04 12:09:40 -0600

    adding efs to git repo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,94 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Eagle Financial</title>
+<head>
+    <title>Eagle Financial Services</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
+    <style>
+        html, body {
+            height: 100%;
+        }
 
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Eagle Financial</div>
-            </div>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 96px;
+        }
+    </style>
+</head>
+<body background="http://www.publicdomainpictures.net/view-image.php?image=121336&picture=sky-blue-textured-background#.WJO4XKKwr6w.link">
+
+
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image class="navbar-brand"  -->
+            <B><h2><a href="{{ url('/customers') }}">
+                        Eagle Financial Services
+                    </a></h2></B>
+            <br>
+
+            <h5>  Funds done Right !</h5>
+
+
+
+
         </div>
-    </body>
+
+    </div>
+</nav>
+<div class="container">
+    <div class="content">
+        <B>  <div class="title">Eagle Financial Services</div></B>
+        <h1><a href="{{ action('CustomerController@index') }}">Click here to proceed</a></h1>
+
+    </div>
+</div>
+
+<footer alignment="Center"  >
+    <p><B>&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp  Posted by: Vaibhav Rahangdale   &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp
+</footer>
+
+
+
+
+
+</body>
 </html>
